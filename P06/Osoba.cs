943b9082ca8946bd6a9d284c3ccb96c12f816ac8
novotnyv.03 <novotnyv.03@spst.eu>
2023-02-14 11:30:09 +0100

    P06 skoro hotovo

---
+++ P06/Osoba.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06
+{
+    internal class Osoba
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public int Hmotnost { get; }
+        private int vyska;
+        public int Vyska
+        {
+            get
+            {
+                return vyska;
+            }
+            set
+            {
+                vyska = value;
+                if (vyska >100 && vyska < 250)
+                {
+                    vyska /= 100;
+                }
+            }
+        }
+
+        public Osoba(string jmeno, string prijmeni, int hmotnost, int vyska) 
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            Hmotnost = hmotnost;
+            Vyska = vyska;
+        }
+
+        public double BMI()
+        {
+
+            double BMI = Hmotnost / (vyska * vyska);
+            return BMI;
+        }
+
+        public string 
+
+
+
+
+
+
+    }
+}
